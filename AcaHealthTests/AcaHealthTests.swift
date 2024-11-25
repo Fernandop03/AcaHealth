fa17caadd22983d13eef82f20065921f5e9d6776
Mario <422066442@pcpuma.acatlan.unam.mx>
2024-11-25 17:35:39 -0600

    Initial Commit

---
+++ AcaHealthTests/AcaHealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  AcaHealthTests.swift
+//  AcaHealthTests
+//
+//  Created by Mario Vega on 25/11/24.
+//
+
+import Testing
+@testable import AcaHealth
+
+struct AcaHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
